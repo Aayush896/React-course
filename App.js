7a95ad405067f24ec023d6495f0af369deeed2d2
Aayush Soni <aayush.soni2000@gmail.com>
2025-11-02 13:13:55 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,32 @@
+// nested structure:
+/**
+ * <div id = "parent">
+ *      <div id = "child">
+ *          <h1> "h1 tag it is"</h1>  
+ *          <h2> "h2 tag it is"</h2>  siblings..
+ *      </div>
+ * </div>
+ */
+
+const parent = React.createElement(
+    "div",
+    { id: "parent" },
+    React.createElement(
+        "div",
+        { id: "child" },
+        [
+            React.createElement("h1", {}, "h1 tag it is"),  // array of children
+            React.createElement("h2",{},"h2 tag it is!")
+        ]
+    )
+);
+
+// const heading = React.createElement("h1",
+//     { id: "heading" },
+//     "Hello world from react!"
+// );
+// console.log(heading) // object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
