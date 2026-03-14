de72a473449d19f29902d9eb1b2796c56b501eb6
Aayush Soni <aayush.soni2000@gmail.com>
2026-03-14 13:44:45 +0530

    react components

--- App.js
+++ App.js
@@ -1,34 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// nested structure:
-/**
- * <div id = "parent">
- *      <div id = "child">
- *          <h1> "h1 tag it is"</h1>  
- *          <h2> "h2 tag it is"</h2>  siblings..
- *      </div>
- * </div>
- */
 
-const parent = React.createElement(
-    "div",
-    { id: "parent" },
-    React.createElement(
-        "div",
-        { id: "child" },
-        [
-            React.createElement("h1", {}, "h1 tag it is, learning react"),  // array of children
-            React.createElement("h2",{},"h2 tag it is!")
-        ]
-    )
+
+//React.createElement => Object => HTMLElement(rendered on the DOM)
+// const heading = React.createElement("h1",{id:"heading"},"React Learning"); // we will not use this from now on, we will use JSX
+
+
+//JSX - HTML like  or XML like syntax
+// JSX (transpiled(converted into a code that browers can understand)  before it reached the JS engine)  => transpiling is done by parcel-> Babel
+// JSX => React.createElement => React Element JS-Object => HTMLElement(rendered on the DOM)
+const heading = (
+<h1 className="head" tabIndex="5">
+    JSX Heading 
+</h1>);
+
+
+// React Component
+// Class based component  - OLD way of writing components
+// function based component - New way of writing components (with hooks)
+
+const Title = () => (
+    <h1 className="head" tabIndex="5">
+        Functional Component Title
+    </h1>
 );
 
-// const heading = React.createElement("h1",
-//     { id: "heading" },
-//     "Hello world from react!"
-// );
-// console.log(heading) // object
+// React Functional Component
+const HeadingComponent = () => (
+    <div id="container">
+        <Title /> 
+        <h1 className="heading">Functional Component Heading</h1>
+    </div>
+);
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent />); // rendering functinal components to the page
 
-root.render(parent);
+
